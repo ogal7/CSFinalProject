29d67181bad65d79d3123f43b6f8345d29470c12
Madstergaard <mostergaard@stuy.edu>
2016-01-11 14:51:22 -0500

    Updated Restaurant.java

---
+++ Restaurant.java
@@ -0,0 +1,21 @@
+/* Restaurant.java represents the individual establishments 
+   in a user's restaurant chain */
+
+
+public class Restaurant {
+
+  // Instance vars
+  private boolean hasRats;
+  private boolean hasFlies;
+  private Manager[] managers;
+  private int starRating;
+  private Chef[] chefs;
+  private Waiter[] waiters;
+  private items[] menu;  
+  private int popularity;  // scale from 1-10
+  private int fanciness;   // scale from 1-10
+  private int healthiness; // scale from 1-10
+  private int size;        // scale from 1-10
+  
+  // 
+}
