a8c790ab3c5ddc6a7888a6f4acf4e6c78de7d408
ogal7 <ogallager@stuy.edu>
2016-01-11 14:50:59 -0500

    hahah

---
+++ Employees.java
@@ -0,0 +1,37 @@
+
+
+public abstract class Employee {
+    
+    //instance vars//
+    private double reqSalary;
+    private double rating;
+    private int speed;
+    private int precision;
+    private int accuracy;
+    
+
+    public Employee () {
+	reqSalary = 75000.0;
+	rating = .8;
+	speed = 7;
+	precision = 7;
+	accuracy = 7;
+    }
+
+
+    public static double getSalary (); 
+    public static void setSalary (double sal);
+
+    public static double getRating();
+    public static void main(String [] args);
+
+
+    public static int getSpeed();
+    public void int setSpeed(int s);
+    
+    
+
+
+    }
+
+}
