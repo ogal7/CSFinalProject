3e3f8f8380c46ebca4722002d37fe2ea0aa2a337
Maddie Ostergaard <mostergaard@stuy.edu>
2016-01-12 18:14:53 -0500

    Updated Chef.java

---
+++ Chef.java
@@ -0,0 +1,22 @@
+public class Chef extends Employee{
+
+  // Instance vars
+  private int cookingKnowledge;
+  
+  // Constructor
+  public Chef(){
+    reqSalary = 75000.0;
+    rating = .8;
+    speed = 7;
+    precision = 7;
+    accuracy = 7;
+    cookingKnowledge = 2;
+  }
+	
+  // Methods
+  public void setCookingKnowledge(int i){
+    greediness = i;}
+  public int getCookingKnowledge(){
+    return cookingKnowledge;}
+	  
+}
