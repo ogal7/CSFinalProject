5f29ad9cbb38bc6f0a2ef17f2de0fb0ebaa96a2a
Maddie Ostergaard <mostergaard@stuy.edu>
2016-01-12 18:27:10 -0500

    Updated DEA.java

---
+++ DEA.java
@@ -0,0 +1,18 @@
+public class DEA{
+
+  //Instance vars
+  private boolean foundDrugs;
+  
+  // Constructor
+  public DEA{
+    foundDrugs = false;}
+  
+  // Methods
+  public void setFoundDrugs(boolean b){
+    foundDrugs = b;}
+  public boolean getFoundDrugs(){
+    return foundDrugs;
+  }
+  
+}
+    
