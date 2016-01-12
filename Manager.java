2f3b432d34979aef31a6b6a74e80b37258104b0e
Maddie Ostergaard <mostergaard@stuy.edu>
2016-01-12 18:10:57 -0500

    Manager inst vars, constructor, get/set methods

---
+++ Manager.java
@@ -0,0 +1,22 @@
+public class Manager extends Employee{
+
+  // Instance vars
+  private int greediness;
+  
+  // Constructor
+  public Manager(){
+    reqSalary = 75000.0;
+	  rating = .8;
+	  speed = 7;
+  	precision = 7;
+	  accuracy = 7;
+	  greediness = 2;
+	}
+	
+	// Methods
+	public void setGreediness(int i){
+	  greediness = i;}
+	public int getGreediness(){
+	  return greediness;}
+	  
+}
