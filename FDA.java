271d34fda7aff45eda29ff9ef22fb8fea71bf032
Maddie Ostergaard <mostergaard@stuy.edu>
2016-01-12 18:37:10 -0500

    Updated FDA.java

---
+++ FDA.java
@@ -0,0 +1,18 @@
+public class FDA{
+
+  //Instance vars
+  private boolean passedInspection;
+  
+  // Constructor
+  public FDA{
+    passedInspection = true;}
+  
+  // Methods
+  public void setPassedInspection(boolean b){
+    passedInspection = b;}
+  public boolean getPassedInspection(){
+    return passedInspection;
+  }
+  
+}
+    
