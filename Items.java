8cb4dad37bfdb430bad6b37833ca6f572f0b8922
ogal7 <ogallager@stuy.edu>
2016-01-13 14:50:23 -0500

    Update Items.java

--- Items.java
+++ Items.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+
 public class Items {
 
   // Instance vars
+  public ArrayList<Object>() directory;
   
+  public Items() {
+    Food a = new Food("Pizza", price, rotting);
+    Food b = new Food("Spaghetti", price, rotting);
+    Food c = new Food("Chicken Burrito", price, rotting);
+    Food d = new Food("Guacamole", price, rotting);
+    Food e = new Food("Sushi", price, rotting);
+    Food f = new Food("Steak", price, rotting);
+    Food g = new Food("Hamburger", price, rotting);
+    Food h = new Food("Veggie Burger", price, rotting);
+    Food i = new Food("Tacos", price, rotting);
+    Food j = new Food("Pancakes", price, rotting);
+    Food k = new Food("Waffles", price, rotting);
+    Food l = new Food("Mozzarella Panini", price, rotting);
+    Food m = new Food("Veggie Panini", price, rotting);
+    Food n = new Food("Caesar Salad", price, rotting);
+    Food o = new Food(name, price, rotting);
+    
+    Bevs aa = new Bev (name, price, maintenance,hasalc);
+    Bevs aa = new Bev (name, price, maintenance,hasalc);
+    Bevs aa = new Bev (name, price, maintenance,hasalc);
+    Bevs aa = new Bev (name, price, maintenance,hasalc);
+    Bevs aa = new Bev (name, price, maintenance,hasalc);
+
+  }
   // Constructor
   
   //Methods
