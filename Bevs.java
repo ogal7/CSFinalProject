1c583c9a43cf61dc8a01d2b2ffa044d71332f414
ogal7 <ogallager@stuy.edu>
2016-01-13 14:39:57 -0500

    Update Bevs.java

--- Bevs.java
+++ Bevs.java
@@ -1,7 +1,29 @@
-public class Bevs extends Items {
+public class Bevs {
 
-  // Instance vars
+  //instance vars
+  String name;
+  int price;
+  int maintenance;
+  boolean hasAlcohol;
   
-  // Constructor
-  
-  //Methods
+public Bevs() {
+	name = "Apple Juice";
+	price = 1;
+	maintenance = 1;
+	hasAlcohol = false;
+}
+
+
+public Bevs(String n, int p, int m, boolean a) {
+	name = n;
+	price = p;
+	maintenance = m;
+  hasAlcohol = a;
+}
+
+public String toString() {
+	String retStr = "";
+	retStr=name + "   ~   $"+ price + "   ~   " + maintenance+"   ~   " + hasAlcohol;	
+	return retStr;
+}
+}
