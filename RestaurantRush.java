6dadd8eee80ef9e07f6ff65020b6ba66651705ac
ogal7 <ogallager@stuy.edu>
2016-01-13 02:00:36 -0500

    Update RestaurantRush.java

--- RestaurantRush.java
+++ RestaurantRush.java
@@ -1,7 +1,25 @@
 public class RestaurantRush {
+  int monthesToGo = 12;
 
   public static void play() {
-  
+    System.out.println("          Welcome to Restaurant Rush!         ");
+    //prompt for name
+    
+    //create restaurant
+    Restaurant game = new Restaurant;
+    
+    //pick workers
+    
+    Food olivia = new Food();
+    String lala = olivia.toString();
+    System.out.println(lala);
+    
+    //prompt which items to choose
+    
+    //play
+    while (monthesToGo > 0 ) {
+      //play
+    }
   
   }
 
