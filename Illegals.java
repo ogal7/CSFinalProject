149e607e6a94b9a11e71d44755c8a67b5be6d09b
Maddie Ostergaard <mostergaard@stuy.edu>
2016-01-15 02:22:36 -0500

    Added weed methods & constructor

--- Illegals.java
+++ Illegals.java
@@ -1,8 +1,18 @@
 public Illegals {
 
 //instance vars
-//weed
+private boolean weed;
 
+// Constructor
+public Illegals(){
+  weed = false;
+}
+
+// methods
+public void setWeed(boolean b){
+  weed = b;}
+public boolean getWeed(){
+  return weed;}
 
 //uml
 
