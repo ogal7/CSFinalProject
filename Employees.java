0b4d72236a919752a57e5b7cab1e10e0cd627e12
Maddie Ostergaard <mostergaard@stuy.edu>
2016-01-16 13:46:29 -0500

    Does not need editing!

--- Employees.java
+++ Employees.java
@@ -9,50 +9,33 @@ public abstract class Employee {
     private int precision;
     private int accuracy;
     
-
-    public Employee () {
-	reqSalary = 75000.0;
-	rating = .8;
-	speed = 7;
-	precision = 7;
-	accuracy = 7;
-    }
-
-
-    public static double getSalary (){
+    // methods
+    public double getSalary (){
     	return salary;}
-    public static void setSalary (double sal){
-    	salary = sal;
-    }
+    public void setSalary (double sal){
+    	salary = sal;}
 
     public double getRating(){
-    	return rating;
-    }
+    	return rating;}
+    public void setRating(double d){
+    	rating = d;}
 
     public static int getSpeed(){
-    	return speed;
-    }
+    	return speed;}
     public void int setSpeed(int s){
-    	speed = s;
-    }
+    	speed = s;}
     
     public static int getPrecision(){
-    	return precision;
-    }
+    	return precision; }
     public void setPrecision(int p){
-    	precision = p;
-    }
+    	precision = p;}
     
     public static int getAccuracy(){
-    	return accuracy;
-    }
+    	return accuracy;}
     public static void setAccuracy(int a){
-    	accuracy = a;
-    }
+    	accuracy = a;}
     
 //uml
-
-     // main method
 
     }
 
