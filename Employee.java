7b69692e40edb1e0a1b4c6947a2fc4c6c35f7966
ogal7 <ogallager@stuy.edu>
2016-01-19 14:01:57 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -16,20 +16,45 @@ public abstract class Employee {
     
     public Employee () {
         // 7 of each job
+        
+        //chefs
         Chef a = new Chef ("Lisa", 100000.0, .9, 8, 8, 8, 9 );
         employeeDir.add(a);
         Chef b = new Chef ("Bart", 50000, .5, 6, 2, 6, 3 );
-        employeeDir.add(a);
-        Chef c = new Chef ("Homer", reqsalary, rating, speed, precision, accuracy, cookingknowledge )
-        employeeDir.add(a);
-        Chef d = new Chef ("Marge", reqsalary, rating, speed, precision, accuracy, cookingknowledge )
-        employeeDir.add(a);
-        Chef e = new Chef ("Spongebob", reqsalary, rating, speed, precision, accuracy, cookingknowledge )
-        employeeDir.add(a);
-        Chef f = new Chef ("Patrick Star", reqsalary, rating, speed, precision, accuracy, cookingknowledge )
-        employeeDir.add(a);
-        Chef g = new Chef ("Squidward", reqsalary, rating, speed, precision, accuracy, cookingknowledge )
-        employeeDir.add(a);
+        employeeDir.add(b);
+        Chef c = new Chef ("Homer", 10000, .1, 1, 5, 2, 1 );
+        employeeDir.add(c);
+        Chef d = new Chef ("Marge", 60000, .6, 2, 7, 7, 2 );
+        employeeDir.add(d);
+        Chef e = new Chef ("Spongebob", 10, .2, 1, 1, 1, 6 );
+        employeeDir.add(e);
+        Chef f = new Chef ("Patrick Star", 5, .1, 1, 1, 1, 2 );
+        employeeDir.add(f);
+        Chef g = new Chef ("Squidward", 120000, .8, 4, 10, 10, 6 );
+        employeeDir.add(g);
+        
+        
+        //managers
+        Manager x = new Manager("Meredith", 100000, .7, 10, 9, 9, 2);
+        employeeDir.add(x);
+        Manager y = new Manager("Cristina", 200000, .9, 10, 10, 10, 5);
+        employeeDir.add(y);
+        Manager z = new Manager("George", 80000, .6, 7, 7, 8, 1);
+        employeeDir.add(z);
+        Manager xx = new Manager("Alex", 80000, .7, 9, 6, 8,6);
+        employeeDir.add(xx);
+        Manager yy= new Manager("Izzie", 80000, .6 , 7, 9, 9, 1);
+        employeeDir.add(yy);
+        Manager zz = new Manager("Miranda Bailey", 300000, .9, 8, 10, 10, 2);
+        employeeDir.add(zz);
+        Manager aa = new Manager("Callie", 100000,  .8, 9, 9, 9, 3);
+        employeeDir.add(aa);
+        
+        //waiters
+        Waiter m = new Waiter();
+        employeeDir.add()
+        
+        
     }
     
     
