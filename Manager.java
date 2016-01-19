70d22d9f7cc754f1db6368b130e0ea601510bfda
ogal7 <ogallager@stuy.edu>
2016-01-19 13:53:11 -0500

    added overloaded cons

--- Manager.java
+++ Manager.java
@@ -7,12 +7,23 @@ public class Manager extends Employee{
   
   // Constructor
   public Manager(){
+    name = "Mr. Mosby";
     reqSalary = 75000.0;
     rating = .8;
     speed = 7;
     precision = 7;
     accuracy = 7;
     greediness = 2;
+  }
+  
+  public Manager(String n, double req, double rat, int s, int prec, int acc, int gred){
+    name = n;
+    reqSalary = req;
+    rating = rat;
+    speed = s;
+    precision = prec;
+    accuracy = acc;
+    greediness = gred;
   }
 	
   // Methods
