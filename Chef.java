e7aeda7fc9e4175cf1a11d9c65a410469e8fbbaf
ogal7 <ogallager@stuy.edu>
2016-01-19 13:09:19 -0500

    added overloaded cons

--- Chef.java
+++ Chef.java
@@ -7,12 +7,24 @@ public class Chef extends Employee{
   
   // Constructor
   public Chef(){
+    name = "bob";
     reqSalary = 75000.0;
     rating = .8;
     speed = 7;
     precision = 7;
     accuracy = 7;
     cookingKnowledge = 2;
+  }
+  
+  
+  public Chef(String n, double req, double ratin, int s, int prec, int acc, int cK ){
+    name = n;
+    reqSalary = req;
+    rating = ratin;
+    speed = s;
+    precision = prec;
+    accuracy = acc;
+    cookingKnowledge = cK;
   }
 	
   // Methods
