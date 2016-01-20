88fd4420d60f6e4747a74c03220cc9add26f6efe
Maddie Ostergaard <Mostergaard@stuy.edu>
2016-01-19 22:24:04 -0500

    Working on V2

--- RestaurantRushV2.java
+++ RestaurantRushV2.java
@@ -1,5 +1,8 @@
-public class RestaurantRushV2 {
+import java.util.ArrayList;
+import cs1.Keyboard; 
+   
 
+public class RestaurantRushV2 {
 
  
   // Main
@@ -9,13 +12,13 @@ public class RestaurantRushV2 {
       // Welcome intro: rules & regs
     System.out.println("          Welcome to Restaurant Rush!\n \n It's the beginning of January, and you want to \n open a Restaurant!");
 
-	System.out.println("What do you want to name your restaurant?");
+	System.out.print("What do you want to name your restaurant? \n \t Name: ");
 	String name = Keyboard.readString();
-	System.out.println("What size do you want the restaurant to be? Please pick a number from 1-10:");
-	int size = Integer.parseInt(keyboard.readString());
-	System.out.println("Please pick a price scale. We will multiply the preset food item prices by this number: ");
-	float priceScale = Float.parseFloat(keyboard.readString());
-	RestaurantV1 starter = new RestaurantV1(name, size, priceScale);
+	System.out.print("What size do you want the restaurant to be? Please pick a number from 1-10: \n \t");
+	int size = Integer.parseInt(Keyboard.readString());
+	System.out.print("Please pick a price scale. We will multiply the preset food item prices by this number: \n \t");
+	float priceScale = Float.parseFloat(Keyboard.readString());
+	RestaurantV2 starter = new RestaurantV2(name, size, priceScale);
 	System.out.println("Alright, before you pick food items you gotta do some budgetting:");
 	System.out.println("You have $100,000 in funds. ");
 	starter.runOperations();
@@ -25,13 +28,14 @@ public class RestaurantRushV2 {
 	System.out.println("Please Enter the number of the rows you wish to add to your menu. You must choose 15 items\n");
 	int ctr = 0;
 	while (ctr < 15) {
-		int itemrow = Integer.parseInt(keyboard.readString());
-		Item og = selection.directory.get(itemrow);
-		menu.add(og);
-		ctr++;
+	    System.out.print("\t" + (ctr + 1) + ": ");
+	    int itemRow = Integer.parseInt(Keyboard.readString());
+	    Item og = selection.directory.get(itemRow);
+	    menu.add(og);
+	    ctr++;
 	}
 	System.out.println("Menu filled. Now let's pick out staff members");
-	
+	System.out.println("");
 	
 
     // Play
@@ -45,4 +49,4 @@ public class RestaurantRushV2 {
   }
 
 }
-Status API Training Shop Blog About Pricing
+
