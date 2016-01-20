ae6f1205587c93a503208d5498f395b8e2d5199c
ogal7 <ogallager@stuy.edu>
2016-01-20 08:29:45 -0500

    debugging

--- Items.java
+++ Items.java
@@ -2,50 +2,71 @@ import java.util.ArrayList;
 
 public class Items {
 
+
   // Instance vars
   public ArrayList<Object> directory;
+  Food a;
+  Food b;
+  Food c;
+  Food d;
+  Food e;
+  Food f;
+  Food g;
+  Food h;
+  Food i;
+  Food j;
+  Food k;
+  Food l;
+  Food m;
+  Food n;
+  Food o;
+  
+  Bev aa;
+  Bev bb;
+  Bev cc;
+  Bev dd;
   
   public Items() {
-    Food a = new Food("Pizza", 4, 1);
+    a = new Food("Pizza", 4, 1);
     directory.add(a);
-    Food b = new Food("Spaghetti", 7, 1);
+    b = new Food("Spaghetti", 7, 1);
     directory.add(b);
-    Food c = new Food("Chicken Burrito", 7, 2);
+    c = new Food("Chicken Burrito", 7, 2);
     directory.add(c);
-    Food d = new Food("Guacamole", 5, 3);
+    d = new Food("Guacamole", 5, 3);
     directory.add(d);
-    Food e = new Food("Sushi", 7, 4);
+    e = new Food("Sushi", 7, 4);
     directory.add(e);
-    Food f = new Food("Steak", 15, 4);
+    f = new Food("Steak", 15, 4);
     directory.add(f);
-    Food g = new Food("Hamburger", 10, 4);
+    g = new Food("Hamburger", 10, 4);
     directory.add(g);
-    Food h = new Food("Veggie Burger", 9, 2);
+    h = new Food("Veggie Burger", 9, 2);
     directory.add(h);
-    Food i = new Food("Chicken Tacos", 7, 2);
+    i = new Food("Chicken Tacos", 7, 2);
     directory.add(i);
-    Food j = new Food("Pancakes", 5, 1);
+    j = new Food("Pancakes", 5, 1);
     directory.add(j);
-    Food k = new Food("Waffles", 5, 1);
+    k = new Food("Waffles", 5, 1);
     directory.add(k);
-    Food l = new Food("Mozzarella Panini", 9, 2);
+    l = new Food("Mozzarella Panini", 9, 2);
     directory.add(l);
-    Food m = new Food("Veggie Panini", 9, 2);
+    m = new Food("Veggie Panini", 9, 2);
     directory.add(m);
-    Food n = new Food("Caesar Salad", 7, 2);
+    n = new Food("Caesar Salad", 7, 2);
     directory.add(n);
-    Food o = new Food("Falafel", 6, 1);
+    o = new Food("Falafel", 6, 1);
     directory.add(o);
     
-    Bev aa = new Bev ("beer", 6, 1, true);
+    aa = new Bev ("beer", 6, 1, true);
     directory.add(aa);
-    Bev bb = new Bev ("Merlot", 10, 2, true);
+    bb = new Bev ("Merlot", 10, 2, true);
     directory.add(bb);
-    Bev cc = new Bev ("Sparkling Water", 1, 1,false);
+    cc = new Bev ("Sparkling Water", 1, 1,false);
     directory.add(cc);
-    Bev dd = new Bev ("Margherita", 7, 3,true);
+    dd = new Bev ("Margherita", 7, 3,true);
     directory.add(dd);
-    Bev ee = new Bev ("Lemonade", 2, 3,false);
+    ee = new Bev ("Lemonade", 2, 3,false);
     directory.add(ee);
 
   }
