8ff7c0240ae9fd1098482f273cdbc690ac97080e
ogal7 <ogallager@stuy.edu>
2016-01-20 08:41:17 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -10,6 +10,31 @@ public class Employee {
     protected int accuracy;
     protected String name;
     protected ArrayList employeeDir = new ArrayList<Employee>();
+    Chef a;
+    Chef b;
+    Chef c;
+    Chef d;
+    Chef e;
+    Chef f;
+    Chef g;
+    
+    
+    Manager x;
+    Manager y;
+    Manager z;
+    Manager xx;
+    Manager yy;
+    Manager zz;
+    Manager aa;
+    
+    
+    Waiter m;
+    Waiter n;
+    Waiter o;
+    Waiter p;
+    Waiter q;
+    Waiter r;
+    Waiter s;
     
     
     //constructor
@@ -17,51 +42,51 @@ public class Employee {
     public Employee () {
         // 7 of each job
         //chefs
-        Chef a = new Chef ("Lisa", 100000.0, .9, 8, 8, 8, 9 );
+        a = new Chef ("Lisa", 100000.0, .9, 8, 8, 8, 9 );
         employeeDir.add(a);
-        Chef b = new Chef ("Bart", 50000, .5, 6, 2, 6, 3 );
+        b = new Chef ("Bart", 50000, .5, 6, 2, 6, 3 );
         employeeDir.add(b);
-        Chef c = new Chef ("Homer", 10000, .1, 1, 5, 2, 1 );
+        c = new Chef ("Homer", 10000, .1, 1, 5, 2, 1 );
         employeeDir.add(c);
-        Chef d = new Chef ("Marge", 60000, .6, 2, 7, 7, 2 );
+        d = new Chef ("Marge", 60000, .6, 2, 7, 7, 2 );
         employeeDir.add(d);
-        Chef e = new Chef ("Spongebob", 10, .2, 1, 1, 1, 6 );
+        e = new Chef ("Spongebob", 10, .2, 1, 1, 1, 6 );
         employeeDir.add(e);
-        Chef f = new Chef ("Patrick Star", 5, .1, 1, 1, 1, 2 );
+        f = new Chef ("Patrick Star", 5, .1, 1, 1, 1, 2 );
         employeeDir.add(f);
-        Chef g = new Chef ("Squidward", 120000, .8, 4, 10, 10, 6 );
+        g = new Chef ("Squidward", 120000, .8, 4, 10, 10, 6 );
         employeeDir.add(g);
         
         //managers
-        Manager x = new Manager("Meredith", 100000, .7, 10, 9, 9, 2);
+        x = new Manager("Meredith", 100000, .7, 10, 9, 9, 2);
         employeeDir.add(x);
-        Manager y = new Manager("Cristina", 200000, .9, 10, 10, 10, 5);
+        y = new Manager("Cristina", 200000, .9, 10, 10, 10, 5);
         employeeDir.add(y);
-        Manager z = new Manager("George", 80000, .6, 7, 7, 8, 1);
+        z = new Manager("George", 80000, .6, 7, 7, 8, 1);
         employeeDir.add(z);
-        Manager xx = new Manager("Alex", 80000, .7, 9, 6, 8,6);
+        xx = new Manager("Alex", 80000, .7, 9, 6, 8,6);
         employeeDir.add(xx);
-        Manager yy = new Manager("Izzie", 80000, .6 , 7, 9, 9, 1);
+        yy = new Manager("Izzie", 80000, .6 , 7, 9, 9, 1);
         employeeDir.add(yy);
-        Manager zz = new Manager("Miranda Bailey", 300000, .9, 8, 10, 10, 2);
+        zz = new Manager("Miranda Bailey", 300000, .9, 8, 10, 10, 2);
         employeeDir.add(zz);
-        Manager aa = new Manager("Callie", 100000,  .8, 9, 9, 9, 3);
+        aa = new Manager("Callie", 100000,  .8, 9, 9, 9, 3);
         employeeDir.add(aa);
         
         //waiters
-        Waiter m = new Waiter("Ted", 50000, .7, 7, 7, 9, 9, false);
+        m = new Waiter("Ted", 50000, .7, 7, 7, 9, 9, false);
         employeeDir.add(m);
-         Waiter n = new Waiter("Robin", 60000, .9, 9, 10, 9, 2, false);
+        n = new Waiter("Robin", 60000, .9, 9, 10, 9, 2, false);
         employeeDir.add(n);
-         Waiter o = new Waiter("Lily", 55000, .8, 7, 10, 7, 4, false);
+        o = new Waiter("Lily", 55000, .8, 7, 10, 7, 4, false);
         employeeDir.add(o);
-         Waiter p = new Waiter("Marshall", 47000, .7, 7, 8, 9, 7, false);
+        p = new Waiter("Marshall", 47000, .7, 7, 8, 9, 7, false);
         employeeDir.add(p);
-         Waiter q = new Waiter("Barney", 40000, .6 , 10, 5, 6, 6, true);
+        q = new Waiter("Barney", 40000, .6 , 10, 5, 6, 6, true);
         employeeDir.add(q);
-         Waiter r = new Waiter("Ranjit", 70000, .9, 6, 9, 8, 2, false);
+        r = new Waiter("Ranjit", 70000, .9, 6, 9, 8, 2, false);
         employeeDir.add(r);
-         Waiter s = new Waiter("Patrice", 65000, .8, 7, 10, 9, 6, false);
+        s = new Waiter("Patrice", 65000, .8, 7, 10, 9, 6, false);
         employeeDir.add(s);
     }
     
