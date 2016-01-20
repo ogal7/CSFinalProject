7e02dc41ce9c57db6292222cb6b0d754fc7f85b0
ogal7 <ogallager@stuy.edu>
2016-01-20 08:35:12 -0500

    Update Items.java

--- Items.java
+++ Items.java
@@ -4,7 +4,7 @@ public class Items {
 
 
   // Instance vars
-  public ArrayList<Object>() directory;
+  public ArrayList<Object>() directory = new ArrayList<Object>();
   Food a;
   Food b;
   Food c;
