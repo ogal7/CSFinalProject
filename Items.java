d02bfeefbdc2c18feff3c63c3bcd107dc0abaff4
Maddie Ostergaard <mostergaard@stuy.edu>
2016-01-20 20:10:34 -0500

    Update Items.java

--- Items.java
+++ Items.java
@@ -2,23 +2,18 @@ import java.util.ArrayList;
 
 public class Items {
 
+ // Instance vars
+  protected String name;
+  protected double price;
+  protected int maintanance;
 
-  // Instance vars
+// Constructor
+public Items(){
+  name = "smoothie";
+  price = 1;
+  maintanance = 1;
+}
   
-  }
-  
-  //Methods
-  public String toString() {
-    String retStr="Here are the items you can put on your menu: \n         FOODS        \n";
-    //creates a directory for the user to pick items from
-    for (int x = 0; x < directory.size() - 5 ; x ++) {
-      retStr+="\n" + x + "~" + directory.get(x);
-    }
-    retStr += "\n      BEVERAGES";
-    for (int x = directory.size() - 5; x< directory.size(); x ++) {
-      retStr+="\n" + x + "~"+ directory.get(x);
-    }
-    return retStr;
-  }
+  // Methods
 }
 
