752c6fe761eb25e35de3e4ce6c1d02542a42230e
Maddie Ostergaard <Mostergaard@stuy.edu>
2016-01-23 18:01:39 -0500

    runOps() almost done woop woop

--- RestaurantV2.java
+++ RestaurantV2.java
@@ -134,7 +134,7 @@ public class RestaurantV2 extends Directory{
 		    ans = Keyboard.readString();
 		}
 	    }
-	}
+	} // END OF MENU EDITS
 		    
 	// EMPLOYEES
 	System.out.print("\n Current Funds: $" + cashMoney);
@@ -143,6 +143,7 @@ public class RestaurantV2 extends Directory{
 	while (ans.equals("y")){
 	    System.out.println("Indicate the department you want to improve (Waitstaff/Chefs/Managers");
 	    ans = Keyboard.readString();
+	    // CHEFS
 	    if (ans.equals("chefs") || ans.equals("Chefs") || ans.equals("chef") || ans.equals("Chef")){
 		System.out.println("These are your chefs: \n" + this.printChefDir() + "\n ** Chefs listed above **");
 		System.out.println("The way to improve your chefs is to send them to culinary school. \n Chefs will be able to work while they are in school, but their salary will increase.\n How many years would you like to send your chefs to culinary school? ");
@@ -150,29 +151,41 @@ public class RestaurantV2 extends Directory{
 		num = Keyboard.readInt;
 		for (i : chefDir){
 		    i.setCookingKnowledge(i.getCookingKnowledge() + num);
-		    i.setSalary(i.getSalary() * (1 + num * .2));
+		    if (i.getCookingKnowledge() > 10) i.setCookingKnowledge(10);
+		    i.setSalary(i.getSalary() * (1 + num * .1));
 		}
 	    }
+	    // WAITERS
 	    if (ans.equals("Waitstaff") || ans.equals("waitstaff")){
 		System.out.println("These are your waiters: \n" + this.printWaiterDir() + "\n ** Waiters listed above **");
-		System.out.println("The way to improve your waitstaff is to traint them. \n Waiters' accuracy and precision will improve, but their salary will increase. \n How many weeks would you like to train your waitstaff? ");
+		System.out.println("The way to improve your waitstaff is to train them. \n Waiters' accuracy and precision will improve, but their salary will increase. \n How many weeks would you like to train your waitstaff? ");
 		System.out.print(" \t # of weeks (0 - 10): ");
 		num = Keyboard.readInt;
 		for (i : waiterDir){
-		    i.setAccuracy(i.getCookingKnowledge() + num);
-		    i.setSalary(i.getSalary() * (1 + num * .2));
+		    i.setAccuracy(i.getAccuracy() + num);
+		    if (i.getAccuracy() > 10) i.setAccuracy(10);
+		    i.setPrecision(i.getPrecision() + num);
+		    if (i.getPrecision() > 10) i.setPrecision(10);
+		    i.setSalary(i.getSalary() * (1 + num * .1));
 		}
 	    }
+	    // MANAGERS
 	    if (ans.equals("Managers") || ans.equals("managers") || ans.equals("manager") || ans.equals("Manager") ){
-		// EDIT STAFF DIR
+		System.out.println("These are your managers: \n" + this.printManagerDir() + "\n ** Managers listed above **");
+		System.out.println("The way to improve your managers is to be nice to them. \n Managers' greediness will decrease and their ratings willg go up, but their salary will increase because you will have to take them out to dinner. \n How many times do you want to take your managers out to dinner? ");
+		System.out.print(" \t # of times (0 - 10): ");
+		num = Keyboard.readInt;
+		for (i : waiterDir){
+		    i.setGreediness(i.getGreediness() - num);
+		    if (i.getGreediness() < 0) i.setGreediness(0);
+		    i.setRating(i.getRating() + (.1 * num))
+		    if (i.getRating() > 1) i.setRating(1);
+		    i.setSalary(i.getSalary() * (1 + num * .1));
+		}
 	    }
 	    System.out.println("Do you want to continue shaking up your staff? (y/n)    ");
-	}
+	} // END OF EMPLOYEE EDITS
 
-	// 
-	System.out.print("Current Funds: $" + cashMoney);
-	System.out.print("Would you like to change the menu? (y/n)     ");
-	ans = Keyboard.readString();
 	
     }
 
