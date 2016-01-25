c8834c56b6fc86493a04051352486da3e78a1714
ogal7 <ogallager@stuy.edu>
2016-01-24 23:13:11 -0500

    Update RestaurantV2.java

--- RestaurantV2.java
+++ RestaurantV2.java
@@ -140,50 +140,75 @@ public class RestaurantV2 extends Directory implements MonthlyPackages{
     }
 
      public void july(){
-     	 System.out.println("\033[37mHAPPY NEW YEAR!!! \n It's January, and everyone's working to keep their resolutions alive. \n This month, you can purchase cleaning supplies (which will keep those pesky FDA inspectors happy), and add a kale salad to your menu! \n But it can only happen in January!");
-	System.out.print("\033[34mDo you want to purchase the January Package for $400? (type y/n): \033[36m");
+     	 System.out.println("\033[37mHAPPY FOURTH OF JULY!!! \n It's July, and everyone's trying to support this awful country. \n This month, you can add festive items to your menu, like Fireworks Ice Cream and Patriotic Ice Cream soda! This is a july only offer so you better get the hot stuff while it lasts!");
+	System.out.print("\033[34mDo you want to purchase the July Package for $400? (type y/n): \033[36m");
 	if (Keyboard.readString() == "y"){
-	    
+	    Food k = new Food("Fireworks Ice Cream", 5, 2, false);
+	    Bev a = new Bev("Patriotic Ice Cream Soda", 6, 2, false);
+	    menu.add(k);
+	    menu.add(a);
+	    rating +=1;
 	}
     }
 
      public void august(){
-     	 System.out.println("\033[37mHAPPY NEW YEAR!!! \n It's January, and everyone's working to keep their resolutions alive. \n This month, you can purchase cleaning supplies (which will keep those pesky FDA inspectors happy), and add a kale salad to your menu! \n But it can only happen in January!");
-	System.out.print("\033[34mDo you want to purchase the January Package for $400? (type y/n): \033[36m");
+     	 System.out.println("\033[37mHip Hip Hurray for Summer! It's August, and everyone's working to keep cool from the heat.\n This month you can add classy items like Rose and Tapas to end the summer with triumph!\n ");
+	System.out.print("\033[34mDo you want to purchase the August Package for $400? (type y/n): \033[36m");
 	if (Keyboard.readString() == "y"){
-	    
+	    Bev r = new Bev("Rose", 8, 1, true);
+	    Food t = new Food("Tapas", 7, 2, false);
+	    menu.add(r);
+	    menu.add(t);
 	}
     }
 
      public void september(){
-     	 System.out.println("\033[37mHAPPY NEW YEAR!!! \n It's January, and everyone's working to keep their resolutions alive. \n This month, you can purchase cleaning supplies (which will keep those pesky FDA inspectors happy), and add a kale salad to your menu! \n But it can only happen in January!");
-	System.out.print("\033[34mDo you want to purchase the January Package for $400? (type y/n): \033[36m");
+     	 System.out.println("\033[37mWell damn it's back to school and work! \n It's September, and everyone's getting back into the swing of things.\n This month, you can purchase Cosmopolitans and Carbonara.");
+	System.out.print("\033[34mDo you want to purchase the September Package for $400? (type y/n): \033[36m");
 	if (Keyboard.readString() == "y"){
-	    
+	    Food x = new Food("Carbonara", 8, 3, false);
+	    Bev y = new Bev("Cosmopolitan", 7, 3, true);
+	    menu.add(x);
+	    menu.add(y);
 	}
     }
 
      public void october(){
-     	 System.out.println("\033[37mHAPPY NEW YEAR!!! \n It's January, and everyone's working to keep their resolutions alive. \n This month, you can purchase cleaning supplies (which will keep those pesky FDA inspectors happy), and add a kale salad to your menu! \n But it can only happen in January!");
-	System.out.print("\033[34mDo you want to purchase the January Package for $400? (type y/n): \033[36m");
+     	 System.out.println("\033[37mHAPPY HALLOWEEN!!\n It's October so prepare to spook your customers with how scarily good your food is! \n This month, you can add Pumpkin Pie, Pumpkin Spice Lattes, and Candy Cake! - This is a limited time offer!");
+	System.out.print("\033[34mDo you want to purchase the October Package for $400? (type y/n): \033[36m");
 	if (Keyboard.readString() == "y"){
-	    
+	    Food x = new Food("Pumpkin Pie", 5, 3, false);
+	    Food z = new Food("Candy Cake", 5, 3, false);
+	    Bev y = new Bev("Pumpkin Spice Latte", 3, 2, false);
+	    menu.add(x);
+	    menu.add(y);
+	    menu.add(z);
 	}
     }
 
      public void november(){
-     	 System.out.println("\033[37mHAPPY NEW YEAR!!! \n It's January, and everyone's working to keep their resolutions alive. \n This month, you can purchase cleaning supplies (which will keep those pesky FDA inspectors happy), and add a kale salad to your menu! \n But it can only happen in January!");
+     	 System.out.println("\033[37mIt's getting colder! \n It's November, and Thanksgiving is coming up! \n This month, you can add a Thanksgiving Dinner to your menu, complete with Turkey, Mashed Potatoes, Gravy, Cranberry Sauce, and Stuffing! And don't forget dessert~Apple Pie is also included! ");
 	System.out.print("\033[34mDo you want to purchase the January Package for $400? (type y/n): \033[36m");
 	if (Keyboard.readString() == "y"){
-	    
+	    Food x = new Food("Thanksgiving Dinner", 20, 4, false);
+	    Food z = new Food("Apple Pie", 5, 3, false);
+	    menu.add(x);
+	    menu.add(z);
 	}
     }
 
      public void december(){
-     	 System.out.println("\033[37mHAPPY NEW YEAR!!! \n It's January, and everyone's working to keep their resolutions alive. \n This month, you can purchase cleaning supplies (which will keep those pesky FDA inspectors happy), and add a kale salad to your menu! \n But it can only happen in January!");
+     	 System.out.println("\033[37mHappy Holidays! Let it go, Let it go! \n It's December, and the cold never bothered you anyway.\n This month, you can add Eggnog, Yule Log Cake, Peppermint Mocha Lattes, and Chili Bread Bowls! This offer only lasts for December!");
 	System.out.print("\033[34mDo you want to purchase the January Package for $400? (type y/n): \033[36m");
 	if (Keyboard.readString() == "y"){
-	    
+	    Food x = new Food("Chili Bread Bowl", 6, 2, false);
+	    Food z = new Food("Yule Log Cake", 5, 3, false);
+	    Bev y = new Bev("Egg Nog", 3, 1, false);
+	    Bev a = new Bev("Peppermint Mocha Latte", 4, 2, false);
+	    menu.add(x);
+	    menu.add(y);
+	    menu.add(z);
+	    menu.add(a);
 	}
     }
     
